e9eb6aa432b3f5957364b931bc8a781731c184ab
unknown <tsyganok.gleb@bk.ru>
2026-07-12 13:03:57 +0300

    dsad

--- server.py
+++ server.py
@@ -49,7 +49,7 @@ async def init_db(app):
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
-        pool = await asyncpg.create_pool(DATABASE_URL, ssl=ssl_context)
+        pool = await asyncpg.create_pool(DATABASE_URL, ssl='require')
     else:
         pool = await asyncpg.create_pool(DATABASE_URL)
         
